f35c052a3cc271a493bfefb3a9be0f0a94dfabee
Travis Cao <43554775+scaotravis@users.noreply.github.com>
2021-01-07 10:55:06 -0600

    update search engine to exclude homepage and 404

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -4,12 +4,12 @@ sitemap: false
 ---
 
 {% assign counter = 0 %}
-var documents = [{% for page in site.pages %}{
+var documents = [{% for page in site.pages %}{% if page.categories contains "Welcome" or page.categories contains "404" %}{% else %}{
     "id": {{ counter }},
     "url": "{{ site.url }}{{ site.baseurl }}{{ page.url }}",
     "title": "{{ page.categories }} - {{ page.title }}",
     "body": "{{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
-    }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
+    }{% if forloop.last %}{% else %}, {% endif %}{% endif %}{% endfor %}];
 
 var idx = lunr(function () {
     this.ref('id')
