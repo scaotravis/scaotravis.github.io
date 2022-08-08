456c61cf6d57a4693374d4d596f906b5ee463b50
Travis Cao <43554775+scaotravis@users.noreply.github.com>
2022-08-08 15:08:19 -0500

    update boot camp page

--- _pages/teaching/su22-phd-stats.md
+++ _pages/teaching/su22-phd-stats.md
@@ -10,8 +10,21 @@ categories: Teaching
 
 **Instructor**: Travis Cao
 
-**Syllabus**: TBA
+[**Syllabus**]({{ site.baseurl }}/downloads/teaching/su22-phd-stats/stats-boot-camp_syllabus.pdf)
 
 #### Schedule and Materials
 
-TBA
+All lectures and office hours take place at **SSW 220**. 
+
+|     Date    |                     Content                     |
+|:-----------:|	:---------------------------------------------- |
+| 8/22 (Mon) <br> 10a-11:30a | Lec 1: Types of Data; Population vs. Sample; Intro to Descriptive Statistics <br> Worksheet (available by EOD of 8/17) | 
+| 8/23 (Tue) <br> 10a-11:30a | Lec 2: Intro to Descriptive Statistics (Cont'd); Sampling <br> Worksheet (available by EOD of 8/22) | 
+| 8/24 (Wed) <br> 11:15a-12:15p | Office Hour |
+| 8/25 (Tue) <br> 10a-11:30a | Lec 3: Probability <br> Worksheet (available by EOD of 8/24) |
+| 8/26 (Fri) <br> 10a-11:30a | Lec 4: Random Variables; Population Distributions <br> Worksheet (available by EOD of 8/25) |
+| 8/29 (Mon) <br> 10a-11:30a | Lec 5: Sampling Distributions <br> Worksheet (available by EOD of 8/26) | 
+| 8/30 (Tue) <br> 10a-11:30a | Lec 6: Hypothesis Testing <br> Worksheet (available by EOD of 8/29) | 
+| 9/2 (Fri) <br> 8:45a-9:45a | Office Hour |
+| 9/2 (Fri) <br> 10a-11:30a | Lec 7: Inference on One Population <br> Worksheet (available by EOD of 9/1) |
+| 9/6 (Tue) <br> 10a-11:30a | Lec 8 (Last Lec): Intro to STATA and R <br> Worksheet (available by EOD of 9/2) |
--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -43,7 +43,7 @@ var documents = [{
     "id": 8,
     "url": "http://0.0.0.0:4000/teaching/su22-phd-stats/",
     "title": "Teaching - Ph.D. Statistics Boot Camp (Summer 2022)",
-    "body": "Statistics Boot Camp for First-Year Social Work Ph. D. Students (Summer 2022): Modality: In-person Instructor: Travis Cao Syllabus: TBA Schedule and Materials: TBA "
+    "body": "Statistics Boot Camp for First-Year Social Work Ph. D. Students (Summer 2022): Modality: In-person Instructor: Travis Cao Syllabus Schedule and Materials: All lectures and office hours take place at SSW 220.       Date   Content         8/22 (Mon) 10a-11:30a   Lec 1: Types of Data; Population vs. Sample; Intro to Descriptive Statistics Worksheet (available by EOD of 8/17)       8/23 (Tue) 10a-11:30a   Lec 2: Intro to Descriptive Statistics (Cont’d); Sampling Worksheet (available by EOD of 8/22)       8/24 (Wed) 11:15a-12:15p   Office Hour       8/25 (Tue) 10a-11:30a   Lec 3: Probability Worksheet (available by EOD of 8/24)       8/26 (Fri) 10a-11:30a   Lec 4: Random Variables; Population Distributions Worksheet (available by EOD of 8/25)       8/29 (Mon) 10a-11:30a   Lec 5: Sampling Distributions Worksheet (available by EOD of 8/26)       8/30 (Tue) 10a-11:30a   Lec 6: Hypothesis Testing Worksheet (available by EOD of 8/29)       9/2 (Fri) 8:45a-9:45a   Office Hour       9/2 (Fri) 10a-11:30a   Lec 7: Inference on One Population Worksheet (available by EOD of 9/1)       9/6 (Tue) 10a-11:30a   Lec 8 (Last Lec): Intro to STATA and R Worksheet (available by EOD of 9/2)   "
     }];
 
 var idx = lunr(function () {
